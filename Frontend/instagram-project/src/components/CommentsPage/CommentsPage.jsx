74b39077af71872ed5e71ae429102ff8140dcc5e
sashagriss <sasha.meduza24@gmail.com>
2024-12-05 09:36:10 +0200

    dfg

--- Frontend/instagram-project/src/components/CommentsPage/CommentsPage.jsx
+++ Frontend/instagram-project/src/components/CommentsPage/CommentsPage.jsx
@@ -1,33 +1,56 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { ChevronLeftIcon } from "@heroicons/react/24/solid";
-
-// import axios from "axios";
-
-// const CommentsPage = () => {
-//   const { postId } = useParams(); // Extract postId from the route
-//   const [comments, setComments] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch comments
-//     axios
-//       .get(`/api/comments/post/${postId}`)
-//       .then((response) => setComments(response.data))
-//       .catch((err) => console.error("Error fetching comments:", err));
-//   }, [postId]);
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import axios from "axios";
+import { Icon } from "@mui/material";
 
 
-const CommentsPage = () => {
-  // const  { postId } = useParams(); // Extract postId from the route
 
+
+const CommentsPage = (PostContent) => {
+  const { postid } = useParams();
+  const [comments, setComments] = useState([])
+
+  const fetchData = async () => {
+    const token = sessionStorage.getItem("authToken");
+    try {
+      const response = await axios.get(
+        `http://85.250.88.33:3006/api/comments/post/${postid}`,
+        {
+          headers: {
+            Authorization:
+              `Bearer ${token}`,
+          },
+        }          
+      );
+      const data= response.data
+      setComments(data)
+      
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData()
+  }, [postid])
 
   return (
-    <div className="w-full">
-      {/* <div className="border-"></div> */}
-      <h1>Hello, World!</h1>
+    <div className="w-full flex flex-col">
+<div className="flex items-center border-b p-2">
+  <ArrowBackIosNewIcon className="mr-2" />
+  <h1 className="flex-grow text-2xl text-center">Comments</h1>
+</div>
+
+
+      {comments.map((comment) => (
+      <div className="pt-2" key={comment._id}>
+          <h1><strong>{comment.authorId}</strong>: {comment.text}</h1>
+      </div>
+      ))}
     </div>
-  );
-};
+  )
+}
+  
 
 export default CommentsPage;
 
